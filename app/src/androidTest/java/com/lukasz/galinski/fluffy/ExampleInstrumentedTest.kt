9a2b89a86e7f2fc1de87f2b5f7f09e6317f9abb8
LukaszPC <galczo230@gmail.com>
2021-11-09 14:00:08 +0100

    Modified app basic theme

--- app/src/androidTest/java/com/lukasz/galinski/fluffy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/lukasz/galinski/fluffy/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.darul.galinski.fluffy", appContext.packageName)
+        assertEquals("com.lukasz.galinski.fluffy", appContext.packageName)
     }
 }
