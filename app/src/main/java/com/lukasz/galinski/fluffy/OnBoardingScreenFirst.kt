be9ae0826cd415b4bacc7058a71caff6d2ba4652
LukaszPC <galczo230@gmail.com>
2021-11-09 15:44:26 +0100

    DVO_01: First onboarding screen layout created. Added default text styles. App font added

--- app/src/main/java/com/lukasz/galinski/fluffy/OnBoardingScreenFirst.kt
+++ app/src/main/java/com/lukasz/galinski/fluffy/OnBoardingScreenFirst.kt
@@ -3,9 +3,9 @@ package com.lukasz.galinski.fluffy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnBoardingScreenFirst : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.onboarding_screen_first_layout)
     }
 }
