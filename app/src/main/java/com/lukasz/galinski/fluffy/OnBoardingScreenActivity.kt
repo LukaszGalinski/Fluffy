f1737c9ee7b0f794f204361ca9fe2c5126e3123a
Łukasz Galiński <lukaszgalinski@MacBook-Pro-ukasz.local>
2021-11-13 13:58:09 +0100

    Onboarding screen two and three added. Activity name updated

--- app/src/main/java/com/lukasz/galinski/fluffy/OnBoardingScreenActivity.kt
+++ app/src/main/java/com/lukasz/galinski/fluffy/OnBoardingScreenActivity.kt
@@ -3,9 +3,9 @@ package com.lukasz.galinski.fluffy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class OnBoardingScreenFirst : AppCompatActivity() {
+class OnBoardingScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.onboarding_screen_first_layout)
+        setContentView(R.layout.onboarding_screen_first_fragment)
     }
 }
