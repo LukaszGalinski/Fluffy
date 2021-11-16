807413f406c32912f33fc1842b4ea35fed19d1ed
Łukasz Galiński <galczo230@gmail.com>
2021-11-16 20:13:57 +0100

    Add directories for simplify project managment

--- app/src/main/java/com/lukasz/galinski/fluffy/view/OnBoardingScreenActivity.kt
+++ app/src/main/java/com/lukasz/galinski/fluffy/view/OnBoardingScreenActivity.kt
@@ -1,11 +1,14 @@
-package com.lukasz.galinski.fluffy
+package com.lukasz.galinski.fluffy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lukasz.galinski.fluffy.R
 
 class OnBoardingScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding_screen_layout)
+
+
     }
 }
