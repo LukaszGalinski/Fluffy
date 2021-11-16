f5bc5bb28a425bbb0fd73fa09ef6d61a4714e0fa
Łukasz Galiński <galczo230@gmail.com>
2021-11-16 21:08:09 +0100

    ViewBinding add. Switching fragments on Onboarding screen added using ViewPager2

--- app/src/main/java/com/lukasz/galinski/fluffy/view/OnBoardingScreenActivity.kt
+++ app/src/main/java/com/lukasz/galinski/fluffy/view/OnBoardingScreenActivity.kt
@@ -1,14 +1,38 @@
 package com.lukasz.galinski.fluffy.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lukasz.galinski.fluffy.R
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.lukasz.galinski.fluffy.databinding.OnboardingScreenLayoutBinding
 
-class OnBoardingScreenActivity : AppCompatActivity() {
+private const val ONBOARDING_PAGES = 3
+class OnBoardingScreenActivity : FragmentActivity() {
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var onboardingBinding: OnboardingScreenLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.onboarding_screen_layout)
+        onboardingBinding = OnboardingScreenLayoutBinding.inflate(layoutInflater)
+        setContentView(onboardingBinding.root)
+        viewPager = onboardingBinding.onboardingFragmentHost
+        buildViewPager()
+    }
 
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = ONBOARDING_PAGES
+        override fun createFragment(position: Int): Fragment = OnboardingFragment(position)
+    }
 
+    private fun buildViewPager(){
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) { super.onBackPressed() }
+        else { viewPager.currentItem = viewPager.currentItem - 1 }
     }
 }
