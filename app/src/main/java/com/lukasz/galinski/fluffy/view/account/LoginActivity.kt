3de7139aa59e279b204c3460b5eb4a8c6be2f031
Łukasz Galiński <galczo230@gmail.com>
2021-11-24 21:29:36 +0100

    Login screen added

---
+++ app/src/main/java/com/lukasz/galinski/fluffy/view/account/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.lukasz.galinski.fluffy.view.account
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lukasz.galinski.fluffy.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen_layout)
+    }
+}
