81b5e3696c03554109a5116688d97b5a280bb0df
Łukasz Galiński <galczo230@gmail.com>
2021-12-01 16:51:03 +0100

    Add word highlighting  on the login screen

--- app/src/main/java/com/lukasz/galinski/fluffy/view/account/LoginScreen.kt
+++ app/src/main/java/com/lukasz/galinski/fluffy/view/account/LoginScreen.kt
@@ -1,11 +1,20 @@
 package com.lukasz.galinski.fluffy.view.account
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.lukasz.galinski.fluffy.R
 import com.lukasz.galinski.fluffy.databinding.LoginScreenFragmentBinding
+
+
+private const val MARKED_SPANS_COUNT = 7
 
 class LoginScreen : Fragment() {
 
@@ -18,5 +27,22 @@ class LoginScreen : Fragment() {
     ): View {
         _loginBinding = LoginScreenFragmentBinding.inflate(inflater)
         return loginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        markSignUpWord()
+    }
+
+    private fun markSignUpWord() {
+        val textInfo = loginBinding.createAccountInfo
+        val spannable = SpannableString(textInfo.text)
+        spannable.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(activity?.applicationContext!!, R.color.purple_primary)),
+            textInfo.text.length - MARKED_SPANS_COUNT, // start
+            textInfo.text.length, // end
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        textInfo.text = spannable
     }
 }
