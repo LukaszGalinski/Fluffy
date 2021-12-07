d8065133eee28df04c5b57a1da222d43dc27b439
Łukasz Galiński <galczo230@gmail.com>
2021-12-07 21:22:20 +0100

    Add register button appearance change based on its state

--- app/src/main/java/com/lukasz/galinski/fluffy/common/ActivityShared.kt
+++ app/src/main/java/com/lukasz/galinski/fluffy/common/ActivityShared.kt
@@ -1,8 +1,29 @@
 package com.lukasz.galinski.fluffy.common
 
 import android.content.Context
+import android.widget.Button
 import android.widget.Toast
+
+private const val INACTIVE_BUTTON_ALPHA = .6F
+private const val ACTIVE_BUTTON_ALPHA = 1F
 
 fun Context.createToast(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun Button.markAs(state: Boolean) {
+    this.isEnabled = state
+}
+
+fun Button.setStateAppearance(){
+    if (this.isEnabled){this.setActiveStateAppearance()}
+    else this.setInactiveStateAppearance()
+}
+
+private fun Button.setActiveStateAppearance(){
+    this.alpha = ACTIVE_BUTTON_ALPHA
+}
+
+private fun Button.setInactiveStateAppearance(){
+    this.alpha = INACTIVE_BUTTON_ALPHA
 }
